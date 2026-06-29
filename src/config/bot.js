df4a56787aee0c0e5f001e5e884c96c47d927fc4
nouille1103 <nouilleki@gmail.com>
2026-06-29 14:11:09 +0000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Schneke",
+        name: "Kiwo ma reine",
         // Activity type number (0 = Playing).
         type: 3,
       },
